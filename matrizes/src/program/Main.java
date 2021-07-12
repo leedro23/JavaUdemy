713428a7d5a4ad66132328c05607c978d29fe369
Leonardo Felix <leonardofelixfelix@gmail.com>
2021-07-12 08:45:25 -0300

    Exercicios Date e Enum

---
+++ matrizes/src/program/Main.java
@@ -0,0 +1,22 @@
+package program;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		List<String> list = new ArrayList<>();
+		
+		list.add("Maria");
+		list.add("Alex");
+		list.add("Bob");
+		list.add("Anna");
+		list.add(2,"Marco");
+	}
+
+}
---
+++ orders/src/entites/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package entites.enums;
+
+public enum OrderStatus {
+	PENDING_PAYMENTS,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED;
+
+}
